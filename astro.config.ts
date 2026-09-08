20a3c74ba25690bd2f225f6c5c999bc719823e96
Dave Nuñez <david.nunez@alumni.stanford.edu>
2026-09-08 12:57:53 -0700

    feat: exclude /dev pages from prod build

--- astro.config.ts
+++ astro.config.ts
@@ -1,55 +1,52 @@
-// @ts-check
-
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import type { AstroIntegration } from "astro";
 import { defineConfig, fontProviders } from "astro/config";
 
-import react from "@astrojs/react";
-
 function excludeDevPages(): AstroIntegration {
-    return {
-        name: "exclude-dev-pages",
-        hooks: {
-            "astro:build:setup": ({ pages, vite }) => {
-                // pages.delete("key")
-            },
-            "astro:build:done": ({ dir, logger }) => {
-                const shouldExcludePage = process.env.BUILD_CONTEXT === "dev";
+	const ansiBlue = "\x1b[34m";
+	const ansiReset = "\x1b[0m";
 
-                if (shouldExcludePage) {
-                    const qaDir = fileURLToPath(new URL("./preview", dir));
-                    if (existsSync(qaDir)) {
-                        rmSync(qaDir, { recursive: true, force: true });
-                    }
-                }
-            },
-        },
-    };
+	return {
+		name: "exclude-dev-pages",
+		hooks: {
+			"astro:build:setup": ({ pages, logger }) => {
+				if (import.meta.env.PROD) {
+					for (const [page, data] of pages.entries()) {
+						if (data.route.route?.match(/^\/dev\//)) {
+							logger.info(`page: ${ansiBlue}${data.component}${ansiReset}`);
+							pages.delete(page);
+						}
+					}
+				}
+			},
+		},
+	};
 }
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [mdx(), sitemap(), react()],
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), react(), excludeDevPages()],
 
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Afacad Flux",
-            cssVariable: "--font-afacad-flux",
-            fallbacks: ["ui-sans-serif", "system-ui", "sans-serif"],
-        },
-        {
-            provider: fontProviders.fontsource(),
-            name: "JetBrains Mono",
-            cssVariable: "--font-jetbrains-mono",
-            fallbacks: ["ui-monospace", "SFMono-Regular", "Menlo", "monospace"],
-        },
-    ],
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Afacad Flux",
+			cssVariable: "--font-afacad-flux",
+			fallbacks: ["ui-sans-serif", "system-ui", "sans-serif"],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "JetBrains Mono",
+			cssVariable: "--font-jetbrains-mono",
+			fallbacks: ["ui-monospace", "SFMono-Regular", "Menlo", "monospace"],
+		},
+	],
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
